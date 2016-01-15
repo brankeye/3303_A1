ac568813c3c1eabc1e5d1dba01105870fd27ce36
Brandon Keyes <brankeye@gmail.com>
2016-01-15 17:17:13 -0500

    Encapsulated networking in a better way.

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,18 @@
-
+import java.net.*;
+import java.io.*;
 
 public class Client {
 
-	Communication comms;
+	protected DatagramPacket sendPacket, receivePacket;
+	protected DatagramSocket sendReceiveSocket;
 	
 	Client() {
-		comms = new Communication("Client");
-	}
-	
-	public void sendAndReceive() {
-		comms.sendAndReceive("WRQ", "test.txt");
-		comms.sendAndReceive("RRQ", "test.txt");
+		try {
+	       sendReceiveSocket = new DatagramSocket();
+	    } catch (SocketException se) {   // Can't create the socket.
+	       se.printStackTrace();
+	       System.exit(1);
+	    }
 	}
 	
 	public static void main(String[] args) {
@@ -18,6 +20,71 @@ public class Client {
 		client.sendAndReceive();
 		System.out.println("Client running...");
 	}
-
 	
+	public void sendAndReceive() {
+		sendAndReceive("WRQ", "test.txt");
+		sendAndReceive("RRQ", "test.txt");
+	}
+
+	public void sendAndReceive(String req, String text) {
+		sendNewPacket(req, text);
+		receiveNewPacket();
+	}
+	
+	public void sendNewPacket(String type, String str) {
+		try {
+			byte msg[] = Request.getByteArray(type, str, Request.Mode.NETASCII);
+			sendPacket = new DatagramPacket(msg, msg.length, 
+											InetAddress.getLocalHost(), 68);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		// print more log info
+		System.out.println("Client: sending a packet containing:\n" + str);
+		System.out.println("Client: Sending packet:");
+	    System.out.println("To host: " + sendPacket.getAddress());
+	    System.out.println("Destination host port: " + sendPacket.getPort());
+	    int len = sendPacket.getLength();
+	    System.out.println("Length: " + len);
+	    System.out.print("Containing: ");
+	    System.out.println(new String(sendPacket.getData(),0,len)); // or could print "s"
+	    
+	    // Send the datagram packet to the server via the send/receive socket. 
+	    try {
+	       sendReceiveSocket.send(sendPacket);
+	    } catch (IOException e) {
+	       e.printStackTrace();
+	       System.exit(1);
+	    }
+	    System.out.println("Client: Packet sent.\n");
+	}
+	
+	public void receiveNewPacket() {
+		byte data[] = new byte[100];
+		receivePacket = new DatagramPacket(data, data.length);
+		
+		try {
+			sendReceiveSocket.receive(receivePacket);
+		} catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		// Process the received datagram.
+	    System.out.println("Client: Packet received:");
+	    System.out.println("From host: " + receivePacket.getAddress());
+	    System.out.println("Host port: " + receivePacket.getPort());
+	    int len = receivePacket.getLength();
+	    System.out.println("Length: " + len);
+	    System.out.print("Containing: ");
+
+	    // Form a String from the byte array.
+	    String received = new String(data,0,len);   
+	    System.out.println(received);
+
+	    // We're finished, so close the socket.
+	    sendReceiveSocket.close();
+	}
 }
